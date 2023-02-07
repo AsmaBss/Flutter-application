0940fdb9fee7863154ac7b263969427c212455c0
Asma Besbes <asma.besbes@esprit.tn>
2023-02-07 11:46:09 +0100

    current-location

---
+++ lib/google-maps.dart
@@ -0,0 +1,73 @@
+import 'dart:async';
+import 'dart:developer';
+import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  Completer<GoogleMapController> _controller = Completer();
+  // on below line we have specified camera position
+  static final CameraPosition _kGoogle = const CameraPosition(
+    target: LatLng(33.9871301997601, 9.64087277564642),
+    zoom: 7.1,
+  );
+
+  // on below line we have created the list of markers
+  final List<Marker> _markers = <Marker>[
+    Marker(
+        markerId: MarkerId('1'),
+        infoWindow: InfoWindow(
+          title: 'My Position',
+        )),
+  ];
+
+  // created method for getting user current location
+  Future<Position> getUserCurrentLocation() async {
+    await Geolocator.requestPermission()
+        .then((value) {})
+        .onError((error, stackTrace) async {
+      await Geolocator.requestPermission();
+      print("ERROR" + error.toString());
+    });
+    return await Geolocator.getCurrentPosition();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color(0xFF0F9D58),
+        // on below line we have given title of app
+        title: Text("Maps"),
+      ),
+      body: Container(
+        child: SafeArea(
+          // on below line creating google maps
+          child: GoogleMap(
+            // on below line setting camera position
+            initialCameraPosition: _kGoogle,
+            // on below line we are setting markers on the map
+            markers: Set<Marker>.of(_markers),
+            // on below line specifying map type.
+            mapType: MapType.normal,
+            // on below line setting user location enabled.
+            myLocationEnabled: true,
+            // on below line setting compass enabled.
+            compassEnabled: true,
+            // on below line specifying controller on map complete.
+            onMapCreated: (GoogleMapController controller) {
+              _controller.complete(controller);
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
